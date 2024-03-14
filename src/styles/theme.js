cd92c4cd4d7bbb8352039d82fc0f34fa0d2b0278
a-honey <jungahyeon0512@khu.ac.kr>
2024-03-15 03:16:32 +0900

    chore: 컬러 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,15 +2,19 @@ import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   colors: {
-    black: '#242529',
+    black: '#000000',
     white: '#ffffff',
-
-    gray: {
-      100: '#F1F5F9',
-      200: '#E2E8F0',
-      300: '#CBD5E1',
-      400: '#94A3B8',
-    },
+    primary: '#0A0A0D',
+    secondary: '#334155',
+    tertiary: '#64748B',
+    brand: '#0D9488',
+    brandBold: '#115E59',
+    info: '#4F46E5',
+    infoBold: '#3730A3',
+    success: '#0D9488',
+    successBold: '#115E59',
+    warning: '#F59E0B',
+    warningBold: '#B45309',
   },
 });
 
