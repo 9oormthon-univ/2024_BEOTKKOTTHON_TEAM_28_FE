33482900af58c0ff7cc13a34c9d5bc99edb7b044
Sohyunnnn <thgus05061@naver.com>
2024-03-15 23:57:45 +0900

    feat: 헤더 레이아웃 구성 및 페이지 이동 구현

--- src/component/common/layout/Header.jsx
+++ src/component/common/layout/Header.jsx
@@ -1,5 +1,32 @@
+import { Flex, Spacer } from '@chakra-ui/react';
+
 const Header = () => {
-  return <header>헤더</header>;
+  return (
+    <header>
+      <Flex>
+        <a style={{ marginRight: '10px' }}>로고</a>
+        <a style={{ marginRight: '10px' }}>스타트업밸리</a>
+        <Spacer />
+        <nav>
+          <a href='/home' style={{ marginRight: '10px' }}>
+            팀 작업 기록
+          </a>
+          <a href='/question-list' style={{ marginRight: '10px' }}>
+            질문 목록
+          </a>
+          <a href='/task-history' style={{ marginRight: '10px' }}>
+            작업 기록
+          </a>
+          <a href='/dashboard' style={{ marginRight: '10px' }}>
+            나의 대시보드
+          </a>
+          <a href='/mypage' style={{ marginRight: '10px' }}>
+            마이페이지
+          </a>
+        </nav>
+      </Flex>
+    </header>
+  );
 };
 
 export default Header;
