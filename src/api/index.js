4bca1563e682f679e5bc213a957ff39e375978d5
a-honey <jungahyeon0512@khu.ac.kr>
2024-03-15 10:34:57 +0900

    feat: install axios and zustand

---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: '',
+  timeout: 5000,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  config.headers['Content-Type'] = 'application/json';
+  config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`;
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      window.location.href = '/login';
+    }
+
+    return await Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
