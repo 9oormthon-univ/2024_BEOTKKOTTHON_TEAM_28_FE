942d2600e30e7952e78b82322046114fd4373363
a-honey <jungahyeon0512@khu.ac.kr>
2024-03-18 01:57:03 +0900

    feat: 팀원 관리 TaskItem toggle 추가

--- src/component/manage/TaskItem.jsx
+++ src/component/manage/TaskItem.jsx
@@ -1,21 +1,38 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Button, Flex } from '@chakra-ui/react';
 
-const TaskItem = () => {
+const TaskItem = ({ isToggled }) => {
   return (
     <Flex
-      justifyContent='space-between'
+      direction='column'
       gap='20px'
       paddingX='24px'
       paddingY='16px'
       background='gray.100'
       borderRadius='8px'
     >
-      <Box className='SubHead-xl'>03.18 12:00 ~ 03.19 13:00</Box>
-      <Box className='Body-lg' color='brandBold'>
-        46시간 34분
-      </Box>
+      <Flex width='100%' justifyContent='space-between' alignItems='center'>
+        <Box className='SubHead-xl'>03.18 12:00 ~ 03.19 13:00</Box>
+        <Box className='Body-lg' color='brandBold'>
+          46시간 34분
+        </Box>
+      </Flex>
+      {isToggled && (
+        <Flex direction='column' alignItems='flex-end'>
+          <Box>
+            업무 종료 내용입니다.업무 종료 내용입니다.업무 종료 내용입니다.업무 종료 내용입니다.업무
+            종료 내용입니다.업무 종료 내용입니다.업무 종료 내용입니다.업무 종료 내용입니다.
+          </Box>
+          <Button paddingX='75px' paddingY='13px' border='1px solid black'>
+            수정하기
+          </Button>
+        </Flex>
+      )}
     </Flex>
   );
+};
+
+TaskItem.propTypes = {
+  isToggled: Boolean,
 };
 
 export default TaskItem;
