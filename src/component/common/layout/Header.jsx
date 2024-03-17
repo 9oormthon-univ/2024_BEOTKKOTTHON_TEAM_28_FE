12de228291ec1e2f90d554822fe03aab57247e40
Sohyunnnn <thgus05061@naver.com>
2024-03-17 22:23:29 +0900

    feat: header 마크업 진행

--- src/component/common/layout/Header.jsx
+++ src/component/common/layout/Header.jsx
@@ -1,29 +1,56 @@
-import { Flex, Link } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+import { Flex, Link, Spacer } from '@chakra-ui/react';
 
 const Header = () => {
+  const [activeLink, setActiveLink] = useState('');
+
+  useEffect(() => {
+    const currentPath = window.location.pathname;
+    setActiveLink(currentPath);
+  }, []);
+
   return (
     <header>
-      <Flex alignItems="center" p="4" bg="#F0F2F4">
-        <Link href="/" mr="4">
+      <Flex alignItems='center' p='15px 182px'>
+        <Link href='/' mr='9px'>
           로고
         </Link>
-        <Link href="/" mr="4">
-          스타트업밸리
-        </Link>
-        <Flex as="nav" gap="20px">
-          <Link href="/home">
+        <Link href='/'>스타트업 밸리</Link>
+        <Spacer />
+        <Flex gap='20px'>
+          <Link
+            href='/home'
+            color={activeLink === '/home' ? '#047857' : 'black'}
+            _hover={{ textDecoration: 'none' }}
+          >
             팀 작업 기록
           </Link>
-          <Link href="/question-list">
+          <Link
+            href='/question-list'
+            color={activeLink === '/question-list' ? '#047857' : 'black'}
+            _hover={{ textDecoration: 'none' }}
+          >
             질문 목록
           </Link>
-          <Link href="/task-history">
+          <Link
+            href='/task-history'
+            color={activeLink === '/task-history' ? '#047857' : 'black'}
+            _hover={{ textDecoration: 'none' }}
+          >
             작업 기록
           </Link>
-          <Link href="/dashboard">
+          <Link
+            href='/dashboard'
+            color={activeLink === '/dashboard' ? '#047857' : 'black'}
+            _hover={{ textDecoration: 'none' }}
+          >
             나의 대시보드
           </Link>
-          <Link href="/mypage">
+          <Link
+            href='/mypage'
+            color={activeLink === '/mypage' ? '#047857' : 'black'}
+            _hover={{ textDecoration: 'none' }}
+          >
             마이페이지
           </Link>
         </Flex>
