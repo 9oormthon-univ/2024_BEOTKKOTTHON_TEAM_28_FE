5ae832fe1ed359b07787a6d9806740560618206a
Sohyunnnn <thgus05061@naver.com>
2024-03-17 22:28:55 +0900

    feat: footer 마크업 진행

--- src/component/common/layout/Footer.jsx
+++ src/component/common/layout/Footer.jsx
@@ -3,15 +3,15 @@ import { Flex, Text } from '@chakra-ui/react';
 const Footer = () => {
   return (
     <footer>
-      <Flex justify='space-between' p='4' bg='#F0F2F4'>
+      <Flex justify='space-between' p='16px 188px' bg='#F0F2F4'>
         <Flex direction='column'>
           <Text>Check our Notion</Text>
           <Text>저희의 협업 이야기를 알고 싶으신가요? 저희의 노션을 방문해보세요</Text>
-          <Text>@2024 startupvally.all rights reserved</Text>
+          <Text mt='8px'>@2024 startupvally.all rights reserved</Text>
         </Flex>
         <Flex direction='column' p='14px 0'>
           <Text fontSize='sm'>Team 000</Text>
-          <Flex>
+          <Flex gap='35px' mt='12px'>
             <Text fontSize='sm'>김영원</Text>
             <Text fontSize='sm'>안재윤</Text>
             <Text fontSize='sm'>박소현</Text>
