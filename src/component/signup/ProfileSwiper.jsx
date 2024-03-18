1a0d1229287747a3f4bda4c08997b321a6d09841
Sohyunnnn <thgus05061@naver.com>
2024-03-18 23:53:57 +0900

    style: 화살표 스타일 변경

--- src/component/signup/ProfileSwiper.jsx
+++ src/component/signup/ProfileSwiper.jsx
@@ -2,6 +2,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation, Pagination } from 'swiper/modules';
+import { Box } from '@chakra-ui/react';
 
 import blueberry from '../../assets/blueberry.png';
 import cabbage from '../../assets/cabbage.png';
@@ -10,26 +11,64 @@ import cucumber from '../../assets/cucumber.png';
 import strawberry from '../../assets/strawberry.png';
 import tomato from '../../assets/tomato.png';
 
+import arrowPrev from '../../assets/prev.png';
+import arrowNext from '../../assets/next.png';
+
 const images = [tomato, cucumber, carrot, strawberry, blueberry, cabbage];
 
 const ProfileSwiper = () => {
   return (
-    <div style={{ width: '100%', maxWidth: '360px' }}>
+    <Box w='100%' maxW='360px' pos='relative'>
       <Swiper
+        className='swiper'
         slidesPerView={3}
         centeredSlides={false}
         spaceBetween={8}
-        navigation={true}
+        navigation={{
+          prevEl: '.swiper-button-prev',
+          nextEl: '.swiper-button-next',
+        }}
         pagination={{ clickable: true }}
         modules={[Navigation, Pagination]}
       >
         {images.map((image, index) => (
           <SwiperSlide key={index}>
-            <img src={image} alt={`image-${index}`} style={{ width: '120px' }} />
+            <img
+              src={image}
+              alt={`image-${index}`}
+              className='swiper-image'
+              style={{ width: '120px', height: 'auto' }}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
-    </div>
+      <img
+        src={arrowPrev}
+        className='swiper-button-prev'
+        alt='Previous'
+        style={{
+          position: 'absolute',
+          top: '49px',
+          left: '-33px',
+          width: '68px',
+          height: '68px',
+          cursor: 'pointer',
+        }}
+      />
+      <img
+        src={arrowNext}
+        className='swiper-button-next'
+        alt='Next'
+        style={{
+          position: 'absolute',
+          top: '49px',
+          right: '-33px',
+          width: '68px',
+          height: '68px',
+          cursor: 'pointer',
+        }}
+      />
+    </Box>
   );
 };
 
