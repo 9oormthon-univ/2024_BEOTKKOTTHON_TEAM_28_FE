1098ebb6d9e1ce31e243604995901a031db07d3f
Sohyunnnn <thgus05061@naver.com>
2024-03-19 16:32:40 +0900

    feat: 선택 시 focus 기능 추가

--- src/component/login/PartItem.jsx
+++ src/component/login/PartItem.jsx
@@ -1,7 +1,7 @@
 import { Flex, Box, Text } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
-const PartItem = ({ part }) => {
+const PartItem = ({ part, isSelected, onClick }) => {
   return (
     <Flex
       w='128px'
@@ -9,17 +9,21 @@ const PartItem = ({ part }) => {
       direction='column'
       gap='16px'
       align='center'
-      border='1px solid #CCD6E3'
+      border={isSelected ? '1.2px solid #065F46' : '1.2px solid #CCD6E3'}
       borderRadius='8px'
+      cursor='pointer'
+      onClick={onClick}
     >
       <Box w='80px' h='80px' borderRadius='2.667px' bg='gray' />
-      <Text>{part}</Text>
+      <Text color={isSelected ? '#065F46' : 'black'}>{part}</Text>
     </Flex>
   );
 };
 
 PartItem.propTypes = {
   part: PropTypes.string.isRequired,
+  isSelected: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default PartItem;
--- src/component/login/PartModal.jsx
+++ src/component/login/PartModal.jsx
@@ -1,8 +1,15 @@
 import { Button, Modal, ModalOverlay, ModalContent, ModalBody, Text, Flex } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 import PartItem from './PartItem';
+import { useState } from 'react';
 
 const PartModal = ({ isOpen, onClose }) => {
+  const [selectedItemIndex, setSelectedItemIndex] = useState(null);
+
+  const handleItemClick = (index) => {
+    setSelectedItemIndex(index);
+  };
+
   return (
     <Modal size='3xl' isOpen={isOpen} onClose={onClose}>
       <ModalOverlay bg='rgba(10, 10, 13, 0.7)' />
@@ -15,14 +22,39 @@ const PartModal = ({ isOpen, onClose }) => {
           <Text>*필수항목입니다.</Text>
 
           <Flex direction='row' gap='15px' justify='center' mt='24px'>
-            <PartItem part='기획' />
-            <PartItem part='디자인' />
-            <PartItem part='프론트엔드' />
-            <PartItem part='백엔드' />
-            <PartItem part='풀스택' />
+            <PartItem
+              part='기획'
+              isSelected={selectedItemIndex === 0}
+              onClick={() => handleItemClick(0)}
+            />
+            <PartItem
+              part='디자인'
+              isSelected={selectedItemIndex === 1}
+              onClick={() => handleItemClick(1)}
+            />
+            <PartItem
+              part='프론트엔드'
+              isSelected={selectedItemIndex === 2}
+              onClick={() => handleItemClick(2)}
+            />
+            <PartItem
+              part='백엔드'
+              isSelected={selectedItemIndex === 3}
+              onClick={() => handleItemClick(3)}
+            />
+            <PartItem
+              part='풀스택'
+              isSelected={selectedItemIndex === 4}
+              onClick={() => handleItemClick(4)}
+            />
           </Flex>
 
-          <Button mt='24px' onClick={onClose} w='100%'>
+          <Button
+            mt='24px'
+            onClick={onClose}
+            w='100%'
+            colorScheme={selectedItemIndex !== null ? 'green' : 'gray'}
+          >
             선택완료
           </Button>
         </ModalBody>
