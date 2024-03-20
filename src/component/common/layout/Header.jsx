5679c2b5f2a8d2e0318d5944c77cc476fd2bc895
a-honey <jungahyeon0512@khu.ac.kr>
2024-03-20 16:53:10 +0900

    chore: 레이아웃 조정

--- src/component/common/layout/Header.jsx
+++ src/component/common/layout/Header.jsx
@@ -18,65 +18,8 @@ const Header = () => {
   const width = useBreakpointValue({ base: '200px', md: 0 });
 
   return (
-    <header style={{ paddingBottom: '70px' }}>
-      <Flex
-        borderBottom='1px solid #B5C1D1'
-        position='fixed'
-        background='white'
-        zIndex='999'
-        width='100%'
-        height='70px'
-        alignItems='center'
-        p='15px 182px'
-      >
-        <Link href='/' mr='9px'>
-          <Flex gap='9px' alignItems='center'>
-            <Image src='/favicon.ico' width='40px' height='40px' />
-            <Image src={text_logo} width='131px' alt='스타트업밸리 로고' />
-          </Flex>
-        </Link>
-
-        <Spacer />
-        <Flex gap='20px' align='center'>
-          <Link
-            className='Body-xl'
-            href='/home'
-            color={activeLink === '/home' ? '#047857' : 'black'}
-            _hover={{ textDecoration: 'none' }}
-          >
-            팀 작업 기록
-          </Link>
-          <Link
-            className='Body-xl'
-            href='/question-list'
-            color={activeLink === '/question-list' ? '#047857' : 'black'}
-            _hover={{ textDecoration: 'none' }}
-          >
-            질문 목록
-          </Link>
-          <Link
-            className='Body-xl'
-            href='/task-history'
-            color={activeLink === '/task-history' ? '#047857' : 'black'}
-            _hover={{ textDecoration: 'none' }}
-          >
-            작업 기록
-          </Link>
-          <Link
-            className='Body-xl'
-            href='/dashboard'
-            color={activeLink === '/dashboard' ? '#047857' : 'black'}
-            _hover={{ textDecoration: 'none' }}
-          >
-            나의 대시보드
-          </Link>
-
-          <Image borderRadius='50%' src={tomato} alt='프로필' width='48px' />
-        </Flex>
-      </Flex>
-    </header>
     <>
-      <header>
+      <header style={{ paddingBottom: '70px' }}>
         <Flex
           borderBottom='1px solid #B5C1D1'
           position='fixed'
