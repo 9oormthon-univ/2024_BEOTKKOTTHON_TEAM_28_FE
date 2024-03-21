225876306c22ec7717ea89beb41902eebfa2556f
Sohyunnnn <thgus05061@naver.com>
2024-03-22 03:18:25 +0900

    feat: 회원가입 api 연결 완료

---
+++ src/api/signup/postUserData.js
@@ -0,0 +1,17 @@
+import { AxiosError } from 'axios';
+import axiosInstance from '..';
+
+const postUserData = async (userData) => {
+  try {
+    const response = await axiosInstance.post('/api/auth/sign-up', userData);
+    console.log('Post User Data Response:', response.data);
+    return response.data;
+  } catch (err) {
+    if (err === AxiosError) {
+      console.error(err);
+      return err.response;
+    }
+  }
+};
+
+export default postUserData;
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,20 +1,97 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AuthBox, InputContent } from '../component/common/mocules';
 import { Button, Checkbox, Flex, Text } from '@chakra-ui/react';
-
 import ProfileSwiper from '../component/signup/ProfileSwiper';
+import postUserData from '../api/signup/postUserData';
 
 const SignupPage = () => {
+  const [nickname, setNickname] = useState('');
+  const [discordId, setDiscordId] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [selectedProfile, setSelectedProfile] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSelectProfile = (profile) => {
+    setSelectedProfile(profile);
+  };
+
+  const handleSignup = async () => {
+    console.log('nickname:', nickname);
+    console.log('discordId:', discordId);
+    console.log('password:', password);
+    console.log('confirmPassword:', confirmPassword);
+
+    if (!nickname || !discordId || !password || !confirmPassword) {
+      alert('모든 필드를 입력해주세요.');
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+
+    const userData = {
+      nickname: nickname,
+      discord_id: discordId,
+      password: password,
+      profile_image: selectedProfile,
+    };
+
+    console.log(userData);
+
+    try {
+      const response = await postUserData(userData);
+      console.log('Signup Response:', response);
+
+      navigate('/login');
+    } catch (error) {
+      console.error('Signup Error:', error);
+      alert('회원가입에 실패하였습니다.');
+    }
+  };
+
   return (
     <main>
       <AuthBox mt='80px' mb='191px'>
         <Text mt='36px' mb='12px' w='100%'>
           프로필 캐릭터
         </Text>
-        <ProfileSwiper />
-        <InputContent label='닉네임' placeholder='닉네임' mt='36px' />
-        <InputContent label='디스코드 아이디' placeholder='discord id' mt='12px' />
-        <InputContent label='비밀번호 입력' placeholder='PW' mt='36px' />
-        <InputContent label='비밀번호 확인' placeholder='PW' mt='36px' />
+        <ProfileSwiper onSelectImage={handleSelectProfile} />
+        <InputContent
+          label='닉네임'
+          placeholder='닉네임'
+          mt='36px'
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+        />
+        <InputContent
+          label='디스코드 아이디'
+          placeholder='discord id'
+          mt='12px'
+          value={discordId}
+          onChange={(e) => setDiscordId(e.target.value)}
+        />
+        <InputContent
+          label='비밀번호 입력'
+          placeholder='PW'
+          mt='36px'
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <InputContent
+          label='비밀번호 확인'
+          placeholder='PW'
+          mt='36px'
+          type='password'
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
         <Flex direction='column' w='100%'>
           <Flex justify='space-between' mt='36px'>
             <Text>이용약관 확인</Text>
@@ -25,7 +102,7 @@ const SignupPage = () => {
             <Checkbox />
           </Flex>
         </Flex>
-        <Button w='100%' p='11px' mt='36px'>
+        <Button w='100%' p='11px' mt='36px' onClick={handleSignup}>
           회원가입
         </Button>
       </AuthBox>
