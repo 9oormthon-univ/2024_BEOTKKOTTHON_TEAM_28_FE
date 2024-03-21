6e7897ba92493a5fd8f30a95e4825e13032aa0d8
Sohyunnnn <thgus05061@naver.com>
2024-03-22 03:28:25 +0900

    feat: 로그인 페이지 api 연결

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,17 +4,30 @@ import { Button, Flex, Text } from '@chakra-ui/react';
 import PartModal from '../component/login/PartModal';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import postCredentials from '../api/login/postCredentials';
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [isPartModalOpen, setIsPartModalOpen] = useState(false);
+  const [discordId, setDiscordId] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleSignUpClick = () => {
     navigate('/signup');
   };
 
-  const handleLoginClick = () => {
-    setIsPartModalOpen(true);
+  const handleLoginClick = async () => {
+    try {
+      const response = await postCredentials(discordId, password);
+      console.log('Login Response:', response);
+
+      setIsPartModalOpen(true);
+      setDiscordId('');
+      setPassword('');
+    } catch (error) {
+      console.error('Login Error:', error);
+      alert('로그인에 실패했습니다. 다시 시도해주세요.');
+    }
   };
 
   const handleClosePartModal = () => {
@@ -25,8 +38,21 @@ const LoginPage = () => {
     <main>
       <Flex h='calc(100vh - 472px)' justify='center' marginTop='100px' align='center'>
         <AuthBox>
-          <InputContent label='디스코드 아이디' placeholder='discord id' mt='36px' />
-          <InputContent label='비밀번호 입력' placeholder='PW' mt='36px' />
+          <InputContent
+            label='디스코드 아이디'
+            placeholder='discord id'
+            mt='36px'
+            value={discordId}
+            onChange={(e) => setDiscordId(e.target.value)}
+          />
+          <InputContent
+            label='비밀번호 입력'
+            placeholder='PW'
+            mt='36px'
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <Text mt='36px' textDecoration='underline' cursor='pointer' onClick={handleSignUpClick}>
             회원가입
           </Text>
