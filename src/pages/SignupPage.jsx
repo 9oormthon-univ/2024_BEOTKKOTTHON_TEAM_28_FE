6cb1f374b438791a6f4a4224da6e537d58418975
Sohyunnnn <thgus05061@naver.com>
2024-03-22 21:04:42 +0900

    style: 회원가입 박스 가운데 정렬

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -57,55 +57,57 @@ const SignupPage = () => {
 
   return (
     <main>
-      <AuthBox mt='80px' mb='191px'>
-        <Text mt='36px' mb='12px' w='100%'>
-          프로필 캐릭터
-        </Text>
-        <ProfileSwiper onSelectImage={handleSelectProfile} />
-        <InputContent
-          label='닉네임'
-          placeholder='닉네임'
-          mt='36px'
-          value={nickname}
-          onChange={(e) => setNickname(e.target.value)}
-        />
-        <InputContent
-          label='디스코드 아이디'
-          placeholder='discord id'
-          mt='12px'
-          value={discordId}
-          onChange={(e) => setDiscordId(e.target.value)}
-        />
-        <InputContent
-          label='비밀번호 입력'
-          placeholder='PW'
-          mt='36px'
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <InputContent
-          label='비밀번호 확인'
-          placeholder='PW'
-          mt='36px'
-          type='password'
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        <Flex direction='column' w='100%'>
-          <Flex justify='space-between' mt='36px'>
-            <Text>이용약관 확인</Text>
-            <Checkbox />
+      <Flex justify='center'>
+        <AuthBox mt='80px' mb='191px'>
+          <Text mt='36px' mb='12px' w='100%'>
+            프로필 캐릭터
+          </Text>
+          <ProfileSwiper onSelectImage={handleSelectProfile} />
+          <InputContent
+            label='닉네임'
+            placeholder='닉네임'
+            mt='36px'
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
+          <InputContent
+            label='디스코드 아이디'
+            placeholder='discord id'
+            mt='12px'
+            value={discordId}
+            onChange={(e) => setDiscordId(e.target.value)}
+          />
+          <InputContent
+            label='비밀번호 입력'
+            placeholder='PW'
+            mt='36px'
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputContent
+            label='비밀번호 확인'
+            placeholder='PW'
+            mt='36px'
+            type='password'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <Flex direction='column' w='100%'>
+            <Flex justify='space-between' mt='36px'>
+              <Text>이용약관 확인</Text>
+              <Checkbox />
+            </Flex>
+            <Flex justify='space-between' mt='12px'>
+              <Text>개인정보 처리방침 확인</Text>
+              <Checkbox />
+            </Flex>
           </Flex>
-          <Flex justify='space-between' mt='12px'>
-            <Text>개인정보 처리방침 확인</Text>
-            <Checkbox />
-          </Flex>
-        </Flex>
-        <Button w='100%' p='11px' mt='36px' onClick={handleSignup}>
-          회원가입
-        </Button>
-      </AuthBox>
+          <Button w='100%' p='11px' mt='36px' onClick={handleSignup}>
+            회원가입
+          </Button>
+        </AuthBox>
+      </Flex>
     </main>
   );
 };
