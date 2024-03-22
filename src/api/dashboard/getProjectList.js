8d54e4e54cdbf45b56df0f84a67b3aa30a2935f3
a-honey <jungahyeon0512@khu.ac.kr>
2024-03-22 22:49:19 +0900

    feat: 임시 api 변경

--- src/api/dashboard/getProjectList.js
+++ src/api/dashboard/getProjectList.js
@@ -3,7 +3,7 @@ import axiosInstance from '..';
 
 const getProjectList = async () => {
   try {
-    const response = await axiosInstance.get(`/teams/retrieve-list`);
+    const response = await axiosInstance.get(`/teams/members/6/retrieve-list`);
     return response.data.data.projectList;
   } catch (err) {
     if (err === AxiosError) {
