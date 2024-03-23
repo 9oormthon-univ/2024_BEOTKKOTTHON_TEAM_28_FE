56e8e6eb7d64ad050bddbaa9188719586f332ef6
a-honey <jungahyeon0512@khu.ac.kr>
2024-03-23 17:27:43 +0900

    feat: 완료된 프로젝트 변경

--- src/component/mypage/ProjectList.jsx
+++ src/component/mypage/ProjectList.jsx
@@ -1,24 +1,25 @@
 import { Flex } from '@chakra-ui/react';
 import ProjectItem from './ProjectItem';
+import PropTypes from 'prop-types';
 import { getProjectList } from '../../api/dashboard';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 import useUserStore from '../../stores/userStore';
 
-const GardenList = () => {
+const GardenList = ({ sort }) => {
   const [data, setData] = useState([]);
   const { id } = useParams();
 
   const { userId } = useUserStore();
   useEffect(() => {
     const fetchData = async () => {
-      const response = await getProjectList(id ?? userId);
+      const response = await getProjectList(id ?? userId, sort);
       setData(response);
     };
 
     fetchData();
-  }, [id, userId]);
+  }, [id, userId, sort]);
 
   return (
     <Flex direction='column' gap='40px'>
@@ -35,6 +36,10 @@ const GardenList = () => {
       ))}
     </Flex>
   );
+};
+
+GardenList.propTypes = {
+  sort: PropTypes.string,
 };
 
 export default GardenList;
