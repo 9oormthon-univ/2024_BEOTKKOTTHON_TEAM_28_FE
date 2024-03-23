beea03b61d61de472188a35b233073231a22a2a9
a-honey <jungahyeon0512@khu.ac.kr>
2024-03-23 14:17:14 +0900

    feat: 동료 평가 api 연결

--- src/api/taskhistory/index.js
+++ src/api/taskhistory/index.js
@@ -3,3 +3,4 @@ export { default as getPeerReview } from './getPeerReview';
 export { default as getMemberList } from './getMemberList';
 export { default as getTeamInfo } from './getTeamInfo';
 export { default as getMemberScrum } from './getMemberScrum';
+export { default as getPeerReviewResult } from './getPeerReviewResult';
