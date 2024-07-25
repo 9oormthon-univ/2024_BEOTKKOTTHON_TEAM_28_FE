f19b05a1f940bfbc787610f7f649d3142c2d7b6d
a-honey <jungahyeon0512@khu.ac.kr>
2024-07-25 21:19:32 +0900

    :bug: Fix: package.json  프록시 서버 추가

--- src/pages/TeamMain/components/Banner2/ContentCard.jsx
+++ src/pages/TeamMain/components/Banner2/ContentCard.jsx
@@ -3,6 +3,12 @@ import { Box, Flex } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
 const ContentCard = ({ isConnected }) => {
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+
   return (
     <Flex
       padding='24px 57px 74px 24px'
@@ -14,7 +20,7 @@ const ContentCard = ({ isConnected }) => {
       height='260px'
     >
       <Box className='Display-sm' textColor='#065F46'>
-        2024.03.14
+        {`${year}.${month}.${date}`}
       </Box>
       {!isConnected ? (
         <Box className='Display-sm'>스타트업밸리로 다른 사람들과 함께 업무를 진행해봐요!</Box>
