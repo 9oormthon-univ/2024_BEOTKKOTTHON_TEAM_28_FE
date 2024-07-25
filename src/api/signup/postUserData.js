9cfa969274a3b97e4d30538934975fef115f424e
a-honey <jungahyeon0512@khu.ac.kr>
2024-07-25 21:40:46 +0900

    :bug: Fix: change api endpoint

--- src/api/signup/postUserData.js
+++ src/api/signup/postUserData.js
@@ -3,7 +3,7 @@ import axiosInstance from '..';
 
 const postUserData = async (userData) => {
   try {
-    const response = await axiosInstance.patch('/auth/sign-up', userData);
+    const response = await axiosInstance.patch('/users', userData);
     return response.data;
   } catch (err) {
     if (err === AxiosError) {
