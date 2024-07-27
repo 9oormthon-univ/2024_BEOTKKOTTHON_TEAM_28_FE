1130820b41f18e5dfd9435b06aea06721dbfaedc
Sohyunnnn <thgus05061@naver.com>
2024-07-27 22:19:36 +0900

    ✨ feat: RnR 모달 api 연결

--- src/pages/MyDashboard/components/RnRContentItem.jsx
+++ src/pages/MyDashboard/components/RnRContentItem.jsx
@@ -4,8 +4,9 @@ import MemberItem from '../../../components/molecules/MemberItem';
 import RnRStatusTag from './RnRStatusTag';
 import ToggleIcon from '../../../components/atoms/ToggleIcon';
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const RnRContentItem = () => {
+const RnRContentItem = ({ nickname, part, memberId, profileImage }) => {
   const [isToggled, setIsToggled] = useState(false);
 
   return (
@@ -23,14 +24,20 @@ const RnRContentItem = () => {
       <Flex justifyContent='space-between' alignItems='center'>
         <Flex alignItems='center' gap='10px'>
           <ToggleIcon isToggled={isToggled} />
-          <MemberItem active={true} />
+          <MemberItem
+            active={true}
+            memberId={memberId}
+            nickname={nickname}
+            part={part}
+            profileImage={profileImage}
+          />
         </Flex>
         <RnRStatusTag />
       </Flex>
       {isToggled && (
         <Flex direction='column' alignItems='flex-end' gap='12px'>
           <Textarea
-            placeholder={`정아현님과의 협업은 어땠나요?`}
+            placeholder={`${nickname}님과의 협업은 어땠나요?`}
             resize='none'
             background='#F0F2F4'
             border='1px solid #CCD6E3'
@@ -48,6 +55,13 @@ const RnRContentItem = () => {
       <Divider />
     </Flex>
   );
+};
+
+RnRContentItem.propTypes = {
+  nickname: PropTypes.string.isRequired,
+  part: PropTypes.string.isRequired,
+  memberId: PropTypes.string.isRequired,
+  profileImage: PropTypes.string.isRequired,
 };
 
 export default RnRContentItem;
--- src/pages/MyDashboard/components/RnRModal.jsx
+++ src/pages/MyDashboard/components/RnRModal.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import {
   Box,
   Flex,
@@ -13,8 +15,24 @@ import {
 import RnRContentItem from './RnRContentItem';
 import saveSVG from '../../../assets/save.svg';
 
+import PropTypes from 'prop-types';
+import useTeamStore from '../../../stores/useTeamStore';
+import { getMemberList } from '../../../api/taskhistory';
+
 const RnRModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { teamName } = useTeamStore();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getMemberList();
+      setData(response);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Box onClick={onOpen} borderBottom='1px solid #334155'>
@@ -25,9 +43,10 @@ const RnRModal = () => {
         <ModalContent borderRadius='16px' minWidth='fit-content'>
           <ModalCloseButton />
           <ModalBody paddingX='40px' paddingY='48px'>
+            {/* 모달바디 안에서 작업하기 */}
             <Flex direction='column'>
               <Box className='Display-sm'>
-                스타트업 밸리를 진행하면서
+                {teamName}를 진행하면서
                 <br />
                 팀원분들은 어떠셨나요?
               </Box>
@@ -35,13 +54,23 @@ const RnRModal = () => {
                 * 동료 평가를 진행하지 않으면, 본인의 평가를 확인할 수 없어요
               </Box>
               <Box marginY='24px' className='SubHead-xl'>
-                스타트업 밸리의 멤버 정아현
+                {/* {teamName}의 멤버 {num} */}
+                {teamName}의 멤버 {data?.data?.memberCount || 0}
               </Box>
             </Flex>
             <Flex direction='column'>
               <RnRContentItem />
               <RnRContentItem />
               <RnRContentItem />
+              {data?.data?.memberList?.map((member) => (
+                <RnRContentItem
+                  key={member.memberId}
+                  memberId={member.memberId}
+                  nickname={member.nickname}
+                  part={member.part}
+                  profileImage={member.profileImage}
+                />
+              ))}
             </Flex>
           </ModalBody>
           <ModalFooter>
@@ -56,6 +85,10 @@ const RnRModal = () => {
       </Modal>
     </>
   );
+};
+
+RnRModal.propTypes = {
+  num: PropTypes.string,
 };
 
 export default RnRModal;
