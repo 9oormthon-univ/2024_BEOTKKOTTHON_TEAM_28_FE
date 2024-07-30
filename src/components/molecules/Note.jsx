3dd87dd6c2a7471dacc36b5094565cd94fbe7de5
a-honey <jungahyeon0512@khu.ac.kr>
2024-07-30 23:59:47 +0900

    ⚡️ Feat: 작업 중인 유저 작업 기록 제출

--- src/components/molecules/Note.jsx
+++ src/components/molecules/Note.jsx
@@ -2,8 +2,10 @@ import { Button, Flex, Image, Input } from '@chakra-ui/react';
 
 import PropTypes from 'prop-types';
 import tomato from '../../assets/tomato.png';
+import { useState } from 'react';
 
-const Note = ({ placeholder = '오늘은 어떤 작업을 기록하시나요?' }) => {
+const Note = ({ handleWorkSubmit, placeholder = '오늘은 어떤 작업을 기록하시나요?' }) => {
+  const [content, setContent] = useState('');
   return (
     <Flex gap='16px'>
       <Image borderRadius='50%' src={tomato} alt='프로필' width='48px' />
@@ -15,8 +17,19 @@ const Note = ({ placeholder = '오늘은 어떤 작업을 기록하시나요?' }
         border='1px solid #CCD6E3'
         borderRadius='8px'
         placeholder={placeholder}
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
       />
-      <Button className='SubHead-xl' height='full' color='white' background='#059669'>
+      <Button
+        onClick={() => {
+          handleWorkSubmit(content);
+        }}
+        className='SubHead-xl'
+        height='full'
+        color='white'
+        background='#059669'
+      >
         작성
       </Button>
     </Flex>
@@ -26,6 +39,7 @@ const Note = ({ placeholder = '오늘은 어떤 작업을 기록하시나요?' }
 Note.propTypes = {
   placeholder: PropTypes.string,
   mb: PropTypes.string,
+  handleWorkSubmit: PropTypes.func,
 };
 
 export default Note;
