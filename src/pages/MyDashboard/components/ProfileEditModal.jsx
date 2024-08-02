3319deb6b531c4bb2cfecc1138085a8c49512e32
Sohyunnnn <thgus05061@naver.com>
2024-08-03 04:15:12 +0900

    ✨ Feat: 에러모달 버튼 이벤트 추가 및 리더 버튼 비활성화

--- src/pages/MyDashboard/components/ProfileEditModal.jsx
+++ src/pages/MyDashboard/components/ProfileEditModal.jsx
@@ -82,20 +82,19 @@ const ProfileEditModal = ({ data }) => {
     }
   };
 
-  const handleClose = () => {
-    if (body.nickname !== data.nickname || body.profileImage !== data.profileImage) {
-      setErrorType('cancel');
-      onErrorOpen();
-    } else {
-      onClose();
-    }
+  const handleWithdrawl = () => {
+    setErrorType('withdrawl');
+    onErrorOpen();
   };
 
-  // 에러 모달을 닫기 위해 사용될 함수
   const handleModalClose = () => {
-    // 에러 모달을 열기 위해 설정한 상태를 변경
     setErrorType('cancel');
     onErrorOpen();
+  };
+
+  const handleCloseBoth = () => {
+    onClose(); // 프로필 모달 닫기
+    onErrorClose(); // 에러 모달 닫기
   };
 
   return (
@@ -162,14 +161,25 @@ const ProfileEditModal = ({ data }) => {
                 </Flex>
               </Box>
               <Divider />
-              <Button width='200px' background='#475569' color='white' mr={3} onClick={handleClose}>
+              <Button
+                width='200px'
+                background='#475569'
+                color='white'
+                mr={3}
+                onClick={handleWithdrawl}
+              >
                 서비스 탈퇴
               </Button>
             </Flex>
           </ModalBody>
         </ModalContent>
       </Modal>
-      <ErrorModal type={errorType} isOpen={isErrorOpen} onClose={onErrorClose} />
+      <ErrorModal
+        type={errorType}
+        isOpen={isErrorOpen}
+        onClose={onErrorClose}
+        onCloseBoth={handleCloseBoth}
+      />
     </>
   );
 };
--- src/pages/MyDashboard/components/ProfileEditModalUserItem.jsx
+++ src/pages/MyDashboard/components/ProfileEditModalUserItem.jsx
@@ -33,7 +33,7 @@ const ProfileEditModalUserItem = ({ memberId, nickname, profileImage, isLeader }
         </Flex>
       </Flex>
       {isLeader ? (
-        <Button className='SubHead-lg' paddingX='16px' background='gray.200'>
+        <Button className='SubHead-lg' paddingX='16px' background='gray.200' isDisabled>
           현재 리더 입니다
         </Button>
       ) : (
